08cad4c64585c07b076e29a633fb9d7d79d4aadf
cohyunoh <coh00@stuy.edu>
2019-09-18 14:51:55 -0400

    added app0

--- flask.py
+++ flask.py
@@ -2,7 +2,6 @@ from flask import Flask
 app = Flask(__name__) #create instance of class Flask
 
 @app.route("/") #assign following fxn to run when root route requested
-
 def hello_world():
     print(__name__) #where will this go?
     return "No hablo queso!"
