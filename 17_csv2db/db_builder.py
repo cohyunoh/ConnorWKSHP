630fb66c26d16f4d101a174868c69aeee5b0e302
cohyunoh <coh00@stuy.edu>
2019-10-09 22:14:23 -0400

    added some comments

--- 17_csv2db/db_builder.py
+++ 17_csv2db/db_builder.py
@@ -18,14 +18,30 @@ c = db.cursor()               #facilitate db ops
 #==========================================================
 command = "CREATE TABLE STUDENTS (name TEXT, age INTEGER, id INTEGER)"          # test SQL stmt in sqlite3 shell, save as string
 c.execute(command)    # run SQL statement
-with open('data/students.csv') as csvfile:
-    studentreader = csv.DictReader(csvfile)
-    rownum = 0
+with open('data/students.csv') as csvfile:      # open csv file
+    studentreader = csv.DictReader(csvfile)     # reads it in as a dictionary where the key is the name of the column and the value is the value in the row
+    #rownum = 0
     for row in studentreader:
         #name = row['name']
         #age = row['age']
         #id = row['id']
-        command = "INSERT INTO STUDENTS VALUES(\"" + row['name'] + "\"" + ", " + row['age'] + ", " + row['id'] + ");"
+        command = "INSERT INTO STUDENTS VALUES(\"" + row['name'] + "\"" + ", " + row['age'] + ", " + row['id'] + ");" # insert data
+        #print(rownum)
+        #rownum += 1
+        c.execute(command)
+#==========================================================
+
+#==========================================================
+command = "CREATE TABLE COURSES (code TEXT, mark INTEGER, id INTEGER)"          # test SQL stmt in sqlite3 shell, save as string
+c.execute(command)    # run SQL statement
+with open('data/courses.csv') as csvfile:      # open csv file
+    studentreader = csv.DictReader(csvfile)    # reads it in as a dictionary where the key is the name of the column and the value is the value in the row
+    #rownum = 0
+    for row in studentreader:
+        #name = row['name']
+        #age = row['age']
+        #id = row['id']
+        command = "INSERT INTO COURSES VALUES(\"" + row['code'] + "\"" + ", " + row['mark'] + ", " + row['id'] + ");"   # insert data
         #print(rownum)
         #rownum += 1
         c.execute(command)
