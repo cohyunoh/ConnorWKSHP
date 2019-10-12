42218e6160ff57e39c882faec42b11a449def1b4
cohyunoh <coh00@stuy.edu>
2019-10-12 13:53:37 -0400

    added comments and grouped it by ids now

--- 18_db-nmcrnch/stu_mean.py
+++ 18_db-nmcrnch/stu_mean.py
@@ -8,13 +8,14 @@ import csv       #facilitate CSV I/O
 DB_FILE = "Talos.db"
 db = sqlite3.connect(DB_FILE) #opens existing file or it makes new one if it does not exit
 c = db.cursor()               #facilitate db ops
-c.execute("CREATE TABLE AVERAGES (id INTEGER, average FLOAT);")
-command = "SELECT name, students.id, avg(mark) FROM students, courses WHERE students.id = courses.id GROUP BY name;"
+c.execute("CREATE TABLE AVERAGES (id INTEGER, average FLOAT);") # Create a data table for the averages
+command = "SELECT name, students.id, avg(mark) FROM students, courses WHERE students.id = courses.id GROUP BY students.id;"
+# ^ This command SELECTS name of the student, the id, and the average of all the marks from courses grouped under the same id
 c.execute(command)
-rows = c.fetchall()
+rows = c.fetchall() # fetches the data from the previous execute command and puts it into a list of lists
 for row in rows:
     print(row)
-    command = "INSERT INTO AVERAGES VALUES(" + row[1] + ", " + row[2] + ");"
+    command = "INSERT INTO AVERAGES VALUES(" + str(row[1]) + ", " + str(row[2]) + ");" # inserts the data into AVERAGES data table
     c.execute(command)
 
 db.commit() #save changes
