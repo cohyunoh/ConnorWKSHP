8c605a2a8dd18188b06b20fe1879572496fca66f
cohyunoh <coh00@stuy.edu>
2019-10-12 13:43:19 -0400

    fixed issue where the command was adding the name instead of id

--- 18_db-nmcrnch/stu_mean.py
+++ 18_db-nmcrnch/stu_mean.py
@@ -8,12 +8,14 @@ import csv       #facilitate CSV I/O
 DB_FILE = "Talos.db"
 db = sqlite3.connect(DB_FILE) #opens existing file or it makes new one if it does not exit
 c = db.cursor()               #facilitate db ops
-
+c.execute("CREATE TABLE AVERAGES (id INTEGER, average FLOAT);")
 command = "SELECT name, students.id, avg(mark) FROM students, courses WHERE students.id = courses.id GROUP BY name;"
 c.execute(command)
 rows = c.fetchall()
 for row in rows:
     print(row)
+    command = "INSERT INTO AVERAGES VALUES(" + str(rows[1]) + "," + str(rows[2]) + ");"
+    c.execute(command)
 
 db.commit() #save changes
 db.close()  #close database
