4d35c8f8bf5e1f96564b908e7f9c0669df987667
cohyunoh <coh00@stuy.edu>
2019-10-15 14:25:44 -0400

    faciltate inserting rows

--- 18_db-nmcrnch/stu_mean.py
+++ 18_db-nmcrnch/stu_mean.py
@@ -8,15 +8,27 @@ import csv       #facilitate CSV I/O
 DB_FILE = "Talos.db"
 db = sqlite3.connect(DB_FILE) #opens existing file or it makes new one if it does not exit
 c = db.cursor()               #facilitate db ops
-c.execute("CREATE TABLE AVERAGES (id INTEGER, average FLOAT);") # Create a data table for the averages
-command = "SELECT name, students.id, avg(mark) FROM students, courses WHERE students.id = courses.id GROUP BY students.id;"
-# ^ This command SELECTS name of the student, the id, and the average of all the marks from courses grouped under the same id
-c.execute(command)
-rows = c.fetchall() # fetches the data from the previous execute command and puts it into a list of lists
-for row in rows:
-    print(row)
-    command = "INSERT INTO AVERAGES VALUES(" + str(row[1]) + ", " + str(row[2]) + ");" # inserts the data into AVERAGES data table
+def stuavg():
+    c.execute("CREATE TABLE AVERAGES (id INTEGER, average FLOAT);") # Create a data table for the averages
+    command = "SELECT name, students.id, avg(mark) FROM students, courses WHERE students.id = courses.id GROUP BY students.id;"
+    # ^ This command SELECTS name of the student, the id, and the average of all the marks from courses grouped under the same id
     c.execute(command)
+    rows = c.fetchall() # fetches the data from the previous execute command and puts it into a list of lists
+    for row in rows:
+        print(row)
+        command = "INSERT INTO AVERAGES VALUES(" + str(row[1]) + ", " + str(row[2]) + ");" # inserts the data into AVERAGES data table
+        c.execute(command)
+
+def add_Rows(ID, code, mark):
+    command = "SELECT * FROM courses"
+    courses = c.execute(command)
+
+    #add vales into courses table
+    command = "INSERT INTO courses VALUES ('" + code + "', " + str(mark) + ", " + str(id) + ");"
+    c.execute(command)
+
+stuavg()
+add_Rows(11, 'calculus', 100)
 
 db.commit() #save changes
 db.close()  #close database
