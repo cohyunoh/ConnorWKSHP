406a15b6b126377f63905b910ed5edc4ac79dfe9
cohyunoh <coh00@stuy.edu>
2020-02-05 20:46:19 -0500

    coded clear button and tested successfully

--- 04_canvas/clickdraw.js
+++ 04_canvas/clickdraw.js
@@ -2,8 +2,13 @@
 // true is rectangle
 var drawType = true;
 
+
+// ===================================================================================
 var toggle = function(e){
+  // grabs the text that shows which mode we in
   var text = document.getElementById("toggletext");
+  // change the boolean to the opposite and change the text correspendingly
+  // false = dot | true = rectangle
   if (drawType == true){
     drawType = false;
     text.innerHTML = "Dot";
@@ -13,5 +18,20 @@ var toggle = function(e){
     text.innerHTML = "Rectangle";
   }
 }
+// assigns the button for toggling with the previous function
 var togglebutton = document.getElementById('toggle');
 togglebutton.addEventListener('click', toggle);
+// ===================================================================================
+
+// ===================================================================================
+var clearScreen = function(e){
+  // gets the canvas
+  var c = document.getElementById("slate");
+  var ctx = c.getContext("2d");
+  // adds white rectangle to clear it
+  ctx.clearRect(0,0,c.width,c.height);
+};
+
+var clearbutton = document.getElementById('clear');
+clearbutton.addEventListener('click', clearScreen)
+// ===================================================================================
