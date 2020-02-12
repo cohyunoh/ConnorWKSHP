4d85cbef480ab72f39fc2f01e56be1b1e0d2ec00
cohyunoh <coh00@stuy.edu>
2020-02-11 22:12:22 -0500

    dpone

--- 06_canvas/dotconn.js
+++ 06_canvas/dotconn.js
@@ -1,6 +1,8 @@
+//Connor Oh & Leia Park
+//SoftDev1 pd9
+//K06 -- Dot Dot Dot
+//2020-02-11
 
-
-var startPath = true;
 var xcoord = null;
 var ycoord = null;
 
@@ -8,29 +10,31 @@ var clearCanvas = function(e){ //clears the canvas
   var c = document.getElementById('playground');
   var ctx = c.getContext("2d");
   ctx.clearRect(0, 0, c.width, c.height);
+  xcoord = null;
+  ycoord = null;
 }
 
 var clearbutton = document.getElementById('clear');
 clearbutton.addEventListener('click', clearCanvas);
 
-var draw = function(e){
+var draw = function(event){
   var c = document.getElementById('playground');
   var ctx = c.getContext("2d");
+  var snapx = event.offsetX //takes a snapshot the current mousecoordinates when it was first pressed
+  var snapy = event.offsetY
   ctx.fillStyle = "#ff0000";
   ctx.beginPath(); //this begins the path for the arc to draw and clears the other paths drawn already
-  ctx.arc(e.clientX - c.offsetLeft - 2.5, e.clientY - c.offsetTop - 2.5, 5, 0, 2 * Math.PI);
+  ctx.arc(event.offsetX, event.offsetY, 5, 0, 2 * Math.PI); //creates dot
   ctx.fill();
-  ctx.beginPath();
-  if(startPath){
-    ctx.fillStyle = "#ff0000";;
-    ctx.moveTo(e.clientX - c.offsetLeft, e.clientY - c.offsetTop);
-    startPath = false;
-  }else{
-    ctx.lineTo(e.clientX - c.offsetLeft, e.clientY - c.offsetTop);
-    startPath = true;
+  ctx.moveTo(snapx, snapy); //moves drawing cursor to the starting coordinates
+  if(xcoord != null && ycoord != null){ //if this is not the first dot then
+    ctx.lineTo(xcoord, ycoord);//draw a line to current coordinates
+    ctx.stroke();//make a stroke
   }
-  ctx.stroke();
+  xcoord = event.offsetX;//always replace the last x and y coords
+  ycoord = event.offsetY;
+
 }
 
 var pen = document.getElementById("playground");
-pen.onmousedown = draw;
+pen.addEventListener('click', draw);
