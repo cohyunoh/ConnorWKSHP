758b967102066862017b4785a328563828fbd1fc
cohyunoh <coh00@stuy.edu>
2020-02-13 14:36:06 -0500

    added comments

--- 07_canvas-anim/anim.js
+++ 07_canvas-anim/anim.js
@@ -1,15 +1,21 @@
-var radius = 0;
-var go = false;
-var request;
-var change = 1;
+//Connor Oh
+//Softdev1 pd9
+//K07 -- They lock us in the tower whenever we get caught
+//2020-02-13
+
+//Global Variables
+var radius = 0; //radius of circle
+var go = false; //tells when to play animation
+var request;	//will store the id when played so we can stop the animation
+var change = 1; //amount to change radius by
 var c = document.getElementById("screen");
 var ctx = c.getContext("2d");
 
 var resize = function(e){
   if (radius <= 2){
-    change = 1;
+    change = 1; //if the radius is too small, then start increasing radius
   }else if(radius >= 300){
-    change = -1;
+    change = -1; //if too big then start decreasing
   }
   radius += change;
   ctx.clearRect(0,0,c.width,c.height);
@@ -17,14 +23,14 @@ var resize = function(e){
   ctx.beginPath();
   ctx.arc(300, 300, radius,0, Math.PI * 2);
   ctx.fill();
-  request = window.requestAnimationFrame(resize);
+  request = window.requestAnimationFrame(resize); //sets the request to a new request
 };
 
 
 var animate = function(e){
   if(!go){
-    window.requestAnimationFrame(resize);
-    go = true;
+    window.requestAnimationFrame(resize); //calls the function recursively
+    go = true; //sets the boolean true
   }
 };
 
