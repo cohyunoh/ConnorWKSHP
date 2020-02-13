454f87b9c796fb9a01b79b70300040d3d30cad50
cohyunoh <coh00@stuy.edu>
2020-02-12 20:36:24 -0500

    it works

--- 07_canvas-anim/anim.js
+++ 07_canvas-anim/anim.js
@@ -1,18 +1,21 @@
 var radius = 0;
-var go = true;
+var go = false;
 var request;
+var change = 1;
+var c = document.getElementById("screen");
+var ctx = c.getContext("2d");
+
 var resize = function(e){
-  if (radius <= 0){
-    radius += 0.1;
-  }else if(radius <= 300){
-    radius -= 0.1;
+  if (radius <= 2){
+    change = 1;
+  }else if(radius >= 300){
+    change = -1;
   }
-  var c = document.getElementById("screen");
-  var ctx = c.getContext("2d");
+  radius += change;
   ctx.clearRect(0,0,c.width,c.height);
   ctx.fillStyle = "#ff0000";
   ctx.beginPath();
-  ctx.arc(c.offsetX + 300, c.offsetY + 300, radius, 0, Math.PI * 2);
+  ctx.arc(300, 300, radius,0, Math.PI * 2);
   ctx.fill();
   request = window.requestAnimationFrame(resize);
 };
@@ -22,8 +25,18 @@ var animate = function(e){
   if(!go){
     window.requestAnimationFrame(resize);
     go = true;
-
   }
 };
 
-var animatebutton = 
+var animatebutton = document.getElementById("go");
+animatebutton.addEventListener('click', animate);
+
+var stop = function(e){
+     if (go){
+         go = false;
+         window.cancelAnimationFrame(request);
+     }
+ };
+
+ var stopButton = document.getElementById("stop");
+ stopButton.addEventListener("click", stop);
