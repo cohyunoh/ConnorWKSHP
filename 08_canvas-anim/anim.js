8fefcb2db8a3ba6fdbec20fce80442512af79cde
cohyunoh <coh00@stuy.edu>
2020-02-13 14:54:17 -0500

    made it so the different buttons work by themselves.

--- 08_canvas-anim/anim.js
+++ 08_canvas-anim/anim.js
@@ -10,8 +10,13 @@ var request;	//will store the id when played so we can stop the animation
 var change = 1; //amount to change radius by
 var c = document.getElementById("screen");
 var ctx = c.getContext("2d");
+var xcoord = 300;
+var ycoord = 300;
+var changex = 1;
+var changey = 1;
 
-var resize = function(e){
+
+var circle = function(e){
   if (radius <= 2){
     change = 1; //if the radius is too small, then start increasing radius
   }else if(radius >= 300){
@@ -23,19 +28,39 @@ var resize = function(e){
   ctx.beginPath();
   ctx.arc(300, 300, radius,0, Math.PI * 2);
   ctx.fill();
-  request = window.requestAnimationFrame(resize); //sets the request to a new request
+  request = window.requestAnimationFrame(circle); //sets the request to a new request
 };
 
+var screensaver = function(e){
+  if(xcoord >= 595){
+    change
+  }
+  ctx.clearRect(0,0,c.width,c.height);
+  ctx.fillStyle = "#ff0000";
+  ctx.beginPath();
+  ctx.arc(xcoord, ycoord, radius,2, Math.PI * 2);
+  ctx.fill();
+};
 
-var animate = function(e){
+var animatecircle = function(e){
   if(!go){
-    window.requestAnimationFrame(resize); //calls the function recursively
+    window.requestAnimationFrame(circle); //calls the function recursively
+    go = true; //sets the boolean true
+  }
+};
+
+var animatesaver = function(e){
+  if(!go){
+    window.requestAnimationFrame(screensaver); //calls the function recursively
     go = true; //sets the boolean true
   }
 };
 
 var animatebutton = document.getElementById("go");
-animatebutton.addEventListener('click', animate);
+animatebutton.addEventListener('click', animatecircle);
+
+var animatebutton = document.getElementById("screensaver");
+animatebutton.addEventListener('click', animatesaver);
 
 var stop = function(e){
      if (go){
