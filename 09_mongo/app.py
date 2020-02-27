1871fe778dc5a0559a9bc7ca1518e68a023aae7a
cohyunoh <coh00@stuy.edu>
2020-02-27 14:39:58 -0500

    updated return statments

--- 09_mongo/app.py
+++ 09_mongo/app.py
@@ -13,31 +13,26 @@ if(collection.count()==0):
 # Specified borough
 def getBorough(borough):
      data = collection.find({"borough": borough})
-     for item in data:
-          return item
+     return data
 
 # Specified zip code
 def getZipCode(zipcode):
      data = collection.find({"address.zipcode": zipcode})
-     for item in data:
-          return item
+     return data
 
 # Specified zip code & grade
 def getZipGrade(zipcode, grade):
      data = collection.find({"address.zipcode": zipcode, "grades.0.grade": grade})
-     for item in data:
-          return item
+     return data
 
 # Specified zip code w/ score below a threshold
 def getZipScore(zipcode, score):
      data = collection.find({"address.zipcode": zipcode, "grades.0.grade": {"$lt": score}})
-     for item in data:
-          return item
+     return data
 
 # Something more clever
 def getCuisine(cuisine):
      data = collection.find({"cuisine": cuisine})
-     for item in data:
-          return item
+     return data
 
 print(getBorough("Queens"))
