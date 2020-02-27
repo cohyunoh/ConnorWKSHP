beb8f24c39a2022eff2e8d6de7dcadb45787a728
cohyunoh <coh00@stuy.edu>
2020-02-27 14:26:47 -0500

    updated app.py

--- 09_mongo/app.py
+++ 09_mongo/app.py
@@ -1,11 +1,37 @@
-from flask import Flask
-app = Flask(__name__) #create instance of class Flask
+from pymongo import MongoClient
 
-@app.route("/") #assign following fxn to run when root route requested
-def hello_world():
-    print(__name__) #where will this go?
-    return "No hablo queso!"
+client = MongoClient('localhost', 27017)
+db = client['test']
+collection = db['restaurants']
 
-if __name__ == "__main__":
-    app.debug = True
-    app.run()
+# Specified borough
+def getBorough(borough):
+     data = collection.find({"borough": borough})
+     for item in data:
+          return item
+
+# Specified zip code
+def getZipCode(zipcode):
+     data = collection.find({"address.zipcode": zipcode})
+     for item in data:
+          return item
+
+# Specified zip code & grade
+def getZipGrade(zipcode, grade):
+     data = collection.find({"address.zipcode": zipcode, "grades.0.grade": grade})
+     for item in data:
+          return item
+
+# Specified zip code w/ score below a threshold
+def getZipScore(zipcode, score):
+     data = collection.find({"address.zipcode": zipcode, "grades.0.grade": {"$lt": score}})
+     for item in data:
+          return item
+
+# Something more clever
+def getCuisine(cuisine):
+     data = collection.find({"cuisine": cuisine})
+     for item in data:
+          return item
+
+print(getBorough("Queens"))
