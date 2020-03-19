a584e749a549568f8c82bce77f9898fd3135dfe8
cohyunoh <coh00@stuy.edu>
2020-03-18 23:19:29 -0400

    preparing for deploy

--- 11_mongoflask/__init__.py
+++ 11_mongoflask/__init__.py
@@ -3,6 +3,8 @@ from utl import movies
 import os
 app = Flask(__name__) #create instance of class Flask
 app.secret_key = os.urandom(32) #generates a secret key for session to start
+DIR = os.path.dirname(__file__)
+DIR += '/'
 
 @app.route("/", methods=["GET","POST"]) #assign following fxn to run when root route requested
 def movie():
