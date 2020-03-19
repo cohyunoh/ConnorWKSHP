82c2efa6e5b3c1e08ca89b3d139dd2f4c755880e
cohyunoh <coh00@stuy.edu>
2020-03-18 22:57:45 -0400

    finished app flask

--- 11_mongoflask/app.py
+++ 11_mongoflask/app.py
@@ -1,11 +1,35 @@
-from flask import Flask
+from flask import Flask, render_template, request, session, redirect, url_for
 from utl import movies
+import os
 app = Flask(__name__) #create instance of class Flask
+app.secret_key = os.urandom(32) #generates a secret key for session to start
 
-@app.route("/") #assign following fxn to run when root route requested
-def showmovie():
-    return "No hablo queso!"
+@app.route("/", methods=["GET","POST"]) #assign following fxn to run when root route requested
+def movie():
+    return render_template('index.html')
 
+@app.route("/year", methods=["POST"])
+def year():
+    if (session):
+        startyear = session['start']
+        endyear = session['end']
+        movies = movies.moviesFromTo(startyear, endyear)
+        return render_template('index.html', yearsmovie = movies)
+    return redirect(url_for('movie'))
+@app.route("/name", methods=["POST"])
+def year():
+    if (session):
+        name = session['name']
+        movies = movies.moviesThisPerformerIn(name)
+        return render_template('index.html', namemovie = movies)
+    return redirect(url_for('movie'))
+@app.route("/genre", methods=["POST"])
+def year():
+    if (session):
+        genre = session['genre']
+        movies = movies.moviesInThisGenre(genre)
+        return render_template('index.html', genremovie = movies)
+    return redirect(url_for('movie'))
 if __name__ == "__main__":
     app.debug = True
     app.run()
