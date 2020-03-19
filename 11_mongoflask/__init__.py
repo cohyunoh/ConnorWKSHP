7a226c3843c32a0decb2262d7e7cd4e0633ced14
cohyunoh <coh00@stuy.edu>
2020-03-19 00:18:09 -0400

    work

--- 11_mongoflask/__init__.py
+++ 11_mongoflask/__init__.py
@@ -8,15 +8,6 @@ from sys import argv
 FILENAME = "utl/movies.json"
 
 
-def load_mongo_json(filename,coll):
-    with open(filename,"r") as jsonfile:
-        stringcontent = jsonfile.read()
-        content = loads(stringcontent)
-        coll.insert(content)
-
-def prune_data(coll):
-    pass
-
 app = Flask(__name__) #create instance of class Flask
 app.secret_key = os.urandom(32) #generates a secret key for session to start
 DIR = os.path.dirname(__file__)
@@ -43,13 +34,5 @@ def genre():
     listmovies = movies.moviesInThisGenre(genre)
     return render_template('index.html', genremovie = listmovies)
 if __name__ == "__main__":
-    client = MongoClient()
-    db_tt4 = client.TobyTop40
-    coll_movies = db_tt4.movies
-    if len(argv) > 1:
-        FILENAME = argv[1]
-    print("loading json data from" , FILENAME )
-    load_mongo_json(FILENAME,coll_movies)
-    print("data successfully loaded\nlook in database 'TobyTop40', collection 'movies'")
     app.debug = True
     app.run(host='0.0.0.0')
