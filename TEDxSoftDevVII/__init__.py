72d48ca0edd12c63bc913418a4a9c7c425d3591e
cohyunoh <coh00@stuy.edu>
2020-03-25 22:10:30 -0400

    started archived version

---
+++ TEDxSoftDevVII/__init__.py
@@ -0,0 +1,10 @@
+from flask import Flask, render_template, request, session, url_for, redirect
+app = Flask(__name__) #create instance of class Flask
+
+@app.route("/") #assign following fxn to run when root route requested
+def homepage():
+    return render_template('home.html')
+
+if __name__ == "__main__":
+    app.debug = False
+    app.run()
