b8379dfc6974b1a88a596529fd7d192e8d5db162
cohyunoh <coh00@stuy.edu>
2020-03-31 14:39:22 -0400

    starting project

---
+++ 14_svg/svg.js
@@ -0,0 +1,68 @@
+//Connor Oh & Jeff Lin & Biraj Chowdhury -- SweetandSourGrapes
+//SoftDev2 pd9
+//K13 --  Ask Circles [Change || Die]
+//2020-03-30
+
+var xmlns = "http://www.w3.org/2000/svg"
+var svg = document.getElementById('vimage'); //gets the svg element
+var clearbutton = document.getElementById("clear"); //gets the clear button element
+
+
+var clearSvg = function(e){ //clears the canvas
+  /*
+  var clearedVimage = document.createElementNS(xmlns,"rect");
+  clearedVimage.setAttribute("width", 500);
+  clearedVimage.setAttribute("height", 500);
+  clearedVimage.setAttribute("style", "fill:white");
+  svg.append(clearedVimage);
+  */
+  while(svg.firstChild){
+	   svg.removeChild(svg.firstChild); //same as last time
+  }
+};
+
+
+
+var drawCircle = function(e){
+  if(e.target == svg){
+      x=e.offsetX;
+      y=e.offsetY;
+      var circle = document.createElementNS(xmlns,"circle");
+      circle.setAttribute ("r",30);
+      circle.setAttribute("cx",x);
+      circle.setAttribute("cy",y);
+      circle.setAttribute("fill", "blue");
+      //big brain move to add event listeners to the circles themselves
+      circle.addEventListener('click', changecircle);
+      svg.appendChild(circle);
+    }
+};
+
+
+var changecircle = function(e){
+  if(e.target.getAttribute("fill") == "blue"){
+    changeColor(e);
+  }else if (e.target.getAttribute("fill") == "aqua"){
+    move(e);
+  }
+}
+
+var changeColor = function(e){
+  //simply just replace the color
+  delete e.fill;
+  //pro tip: .target holds the attributes
+  e.target.setAttribute("fill", "aqua");
+};
+
+var move = function(e){
+    delete e.fill;
+    e.target.setAttribute("fill", "blue");
+    var x = Math.floor(Math.random() * 500);
+    var y = Math.floor(Math.random() * 500);
+    e.target.setAttribute("cx", x);
+};
+
+
+
+clearbutton.addEventListener('click', clearSvg);
+svg.addEventListener('click',drawCircle);
