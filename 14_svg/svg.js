4f3742e6b910bc809e1a82ee7908bd8b1fbe1d2c
cohyunoh <coh00@stuy.edu>
2020-03-31 14:43:00 -0400

    added grace's code for the ball moving

--- 14_svg/svg.js
+++ 14_svg/svg.js
@@ -1,68 +1,82 @@
-//Connor Oh & Jeff Lin & Biraj Chowdhury -- SweetandSourGrapes
-//SoftDev2 pd9
-//K13 --  Ask Circles [Change || Die]
-//2020-03-30
+var svg = document.getElementById("vimage");
+var cl = document.getElementById("clear");
+var mv = document.getElementById("move");
+var xt = document.getElementById("extra");
 
-var xmlns = "http://www.w3.org/2000/svg"
-var svg = document.getElementById('vimage'); //gets the svg element
-var clearbutton = document.getElementById("clear"); //gets the clear button element
+var requestID = 0;
+var radius = 10;
 
-
-var clearSvg = function(e){ //clears the canvas
-  /*
-  var clearedVimage = document.createElementNS(xmlns,"rect");
-  clearedVimage.setAttribute("width", 500);
-  clearedVimage.setAttribute("height", 500);
-  clearedVimage.setAttribute("style", "fill:white");
-  svg.append(clearedVimage);
-  */
-  while(svg.firstChild){
-	   svg.removeChild(svg.firstChild); //same as last time
-  }
-};
-
-
-
-var drawCircle = function(e){
-  if(e.target == svg){
-      x=e.offsetX;
-      y=e.offsetY;
-      var circle = document.createElementNS(xmlns,"circle");
-      circle.setAttribute ("r",30);
-      circle.setAttribute("cx",x);
-      circle.setAttribute("cy",y);
-      circle.setAttribute("fill", "blue");
-      //big brain move to add event listeners to the circles themselves
-      circle.addEventListener('click', changecircle);
-      svg.appendChild(circle);
-    }
-};
-
-
-var changecircle = function(e){
-  if(e.target.getAttribute("fill") == "blue"){
-    changeColor(e);
-  }else if (e.target.getAttribute("fill") == "aqua"){
-    move(e);
+var draw = function(e) {
+  if (e.target == svg) {
+    //console.log(e.target)
+    var c = document.createElementNS("http://www.w3.org/2000/svg", "circle");
+    var x = e.offsetX;
+    var y = e.offsetY;
+    c.setAttribute("cx", x);
+    c.setAttribute("cy", y);
+    c.setAttribute("r", radius);
+    c.setAttribute("fill", "blue");
+    c.setAttribute("stroke", "black");
+    c.setAttribute("xchange", 1);
+    c.setAttribute("ychange", 1);
+    c.addEventListener('click', circle);
+    svg.appendChild(c);
   }
 }
 
-var changeColor = function(e){
-  //simply just replace the color
-  delete e.fill;
-  //pro tip: .target holds the attributes
-  e.target.setAttribute("fill", "aqua");
-};
-
-var move = function(e){
-    delete e.fill;
-    e.target.setAttribute("fill", "blue");
+var circle = function(e){
+  if (e.target.getAttribute("fill") == "blue"){
+    e.target.setAttribute("fill", "violet");
+  }
+  else if (e.target.getAttribute("fill") == "violet") {
     var x = Math.floor(Math.random() * 500);
     var y = Math.floor(Math.random() * 500);
     e.target.setAttribute("cx", x);
-};
+    e.target.setAttribute("cy", y);
+    e.target.setAttribute("fill", "blue");
+  }
+}
 
+var move = function() {
+  var circles = document.getElementsByTagName("circle");
+  //console.log(circles);
+  for (var i = 0; i < circles.length; i++) {
+    var cir = circles[i];
+    //console.log(typeof cir.getAttribute("cx"));
+    var x = parseInt(cir.getAttribute("cx"));
+    var y = parseInt(cir.getAttribute("cy"));
+    var xchange = parseInt(cir.getAttribute("xchange"));
+    var ychange = parseInt(cir.getAttribute("ychange"));
+    console.log(typeof svg.getAttribute("width"));
+    if (x > parseInt(svg.getAttribute("width")) - radius) {
+      cir.setAttribute("xchange", -1);
+    } else if (x < radius) {
+      cir.setAttribute("xchange", 1);
+    }
+    if (y > parseInt(svg.getAttribute("height")) - radius) {
+      cir.setAttribute("ychange", -1);
+    } else if (y < radius) {
+      cir.setAttribute("ychange", 1);
+    }
+    x += xchange;
+    y += ychange;
+    cir.setAttribute("cx", x);
+    cir.setAttribute("cy", y);
+  }
+  if (requestID != 0) {
+    requestID = window.requestAnimationFrame(move);
+  }
+}
 
+var clear = function(e){
+  while (svg.lastChild){
+    svg.removeChild(svg.lastChild);
+  }
+}
 
-clearbutton.addEventListener('click', clearSvg);
-svg.addEventListener('click',drawCircle);
+cl.addEventListener('click', clear);
+svg.addEventListener('click', draw);
+mv.addEventListener('click', function(e) {
+  window.cancelAnimationFrame(requestID)
+  requestID = window.requestAnimationFrame(move);
+})
