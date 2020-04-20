e46854de3015b22460c2d85b08a47dbb81b9c232
cohyunoh <coh00@stuy.edu>
2020-04-20 13:46:29 -0400

    fixed a possible bug

--- 18_d3/static/d3.js
+++ 18_d3/static/d3.js
@@ -5,18 +5,16 @@ var render = function(e){
   var numstudents = []
   var i;
   for(i=0; i < data.length; i++){
-    numstudents.push(data[i][0];
+    numstudents.push(data[i][0]);
   };
   visualize(numstudents);
 };
 
 var transition = function(e){
-  var stats = [][][]
+  var stats = []
   var i;
   for(i=0; i < data.length; i++){
-    stats[i][0] = data[i][1];
-    stats[i][1] = data[i][2];
-    stats[i][2] = data[i][3];
+    stats.push([data[i][1],data[i][2],data[i][3]);
   };
   d3.select("body").transition(visualize(stats));
 };
